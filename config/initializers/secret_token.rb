82842f9a809239f0605b7ed7dfd0ac05fd83eede
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-22 22:26:46 -0500

    playing with rails tools in prep for Ruby DCamp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dcamp::Application.config.secret_token = '80ff2dce12d6feccec06866cda05ae08e613524cc80c97480c6a99ba4d843f57483d6363cfd5905299d4af7c8139a01898d273e376df19b2d4c6731a6d284053'
